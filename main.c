3bd75abc69431b41c77eab76cd1e35939d61efeb
Trif Robert <robert.trif@student.upt.ro>
2025-06-02 22:08:46 +0300

    canMove() implementation. Modified handleInput()

--- main.c
+++ main.c
@@ -14,14 +14,10 @@
 
 //------------------------TETRIMINO-----------------------//
 //     1      110      111      10         11
-// I - 1  Z - 011  T - 010  L - 10  cube - 11
-//     1                        11         
-//     1
+// I - 1  Z - 011  T - 010  L - 11  cube - 11
+//        
+//     
 //--------------------------------------------------------//
-
-
-#define ROWS 15
-#define COLUMNS 15
 
 typedef struct{
     int rows;
@@ -29,29 +25,33 @@ typedef struct{
     const int *data;
 }Tetrimino;
 
+#define ROWS 15
+#define COLUMNS 15
 
-const int I_SHAPE[] = {1, 1, 1, 1};
+#define RANDOM(min, max) ((rand() % ((max) - (min) + 1)) + (min))
+
+const int I_SHAPE[] = {1, 1};
 const int Z_SHAPE[] = {1, 1, 0, 0, 1, 1};
 const int T_SHAPE[] = {1, 1, 1, 0, 1, 0};
-const int L_SHAPE[] = {1, 0, 1, 0, 1, 1};
+const int L_SHAPE[] = {1, 0, 1, 1};
 const int CUBE[] = {1, 1, 1, 1};
 
 int environment[ROWS][COLUMNS];
 unsigned char game_running_flag = 1;
 unsigned char special_char_flag = 0;
+unsigned char isTetriminoActive = 0;
 
+int current_row;
+int current_column;
+
+Tetrimino tetrimino;
 Tetrimino tetriminos[] = {
-    {4, 1, I_SHAPE},
+    {2, 1, I_SHAPE},
     {2, 3, Z_SHAPE},
     {2, 3, T_SHAPE},
-    {3, 2, L_SHAPE},
+    {2, 2, L_SHAPE},
     {2, 2, CUBE}
 };
-
-int returnRandom(int min, int max){
-    int rd_num = rand() % (max - min + 1) + min;
-    return rd_num;
-}
 
 // Windows version of readInput
 int readInput() {
@@ -72,15 +72,34 @@ void printBoard() {
 }
 
 Tetrimino createTetrimino(){
-    return tetriminos[returnRandom(0,4)];
+    return tetriminos[RANDOM(0,4)];
 }
 
-void insertTetrimino(Tetrimino tetrimino, int insertion_row, int insertion_column){
+int canMove(Tetrimino tetrimino, int row, int column){
+
+    if((tetrimino.rows + row) > ROWS || (tetrimino.columns + column) > COLUMNS
+            || (row < 0) || (column < 0)){
+        return 0;
+    }
+    
+    for(int i=0; i< tetrimino.rows ; i++){
+        for(int j=0 ; j<tetrimino.columns; j++){
+            if((*(tetrimino.data + i * tetrimino.columns + j)) == 1){
+                if(environment[row+i][column+j] == 1){
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+void insertTetrimino(Tetrimino tetrimino, int row, int column){
 
     for(int i = 0 ; i < tetrimino.rows ; i++){
         for(int j = 0 ; j < tetrimino.columns ; j++){
             int value = *(tetrimino.data + i * tetrimino.columns + j);
-            environment[insertion_row+i][insertion_column+j] = value;
+            environment[row+i][column+j] = value;
         }
     }
 }
@@ -89,9 +108,16 @@ void handleInput(){
     int input = readInput();
     printf("Input: %d\n", input);
 
+    if(isTetriminoActive == 0){
+        tetrimino = createTetrimino();
+        insertTetrimino(tetrimino, current_row, current_column);
+        isTetriminoActive = 1;
+    }
+
     if (input == 'q') {
         //exit game
         game_running_flag = 0;
+        return;
     }
     else if(input == 32){
         //space key
@@ -103,20 +129,27 @@ void handleInput(){
     else if(special_char_flag == 1){
         if(input == 77){
             //right key
+            if(canMove(tetrimino, current_row, current_column+1)){
+                current_column++;
+            }   
         }
         else if(input == 75){
             //left key
+            if(canMove(tetrimino, current_row, current_column-1)){
+                current_column--;
+            }
         }
         else if(input ==  72){
             //up key
         }
         else if(input == 80){
-            //down key
+            if(canMove(tetrimino, current_row+1, current_column)){
+                current_row++;
+            }
         }
         special_char_flag = 0;
     }
-    Tetrimino new_tetrimino = createTetrimino();
-    insertTetrimino(new_tetrimino, 0, 6);
+    insertTetrimino(tetrimino, current_row, current_column);
 }
 
 int main() {
@@ -125,8 +158,8 @@ int main() {
         system("cls");
         handleInput();
         printBoard();
-        memset(environment, 0, sizeof(environment));    //clean the environment
-        Sleep(500); // milliseconds
+        memset(environment, 0, sizeof(environment));
+        Sleep(300); // milliseconds
     }
 
     return 0;
